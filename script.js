ce41e2d8960e88eb655dbe98609889537fb3e18b
sage-sama <56013544+sage-sama@users.noreply.github.com>
2020-10-16 08:09:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+function toggleDarkLightMode(isDark){
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 /50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 /50%' : 'rgb(0 0 0 / 50%';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light');
+}
+
 // FOr dark or light images
 function imageMode(color) {
     image1.src = `img/undraw_proud_coder_${color}.svg`;
@@ -13,38 +21,22 @@ function imageMode(color) {
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 /50%';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
-}
-
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 /50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
-}
-
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
-        darkMode();
+        toggleDarkLightMode(true);
     } else{
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
 
-// CHeck local storage
+// Check local storage
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme){
     document.documentElement.setAttribute('data-theme', currentTheme);
